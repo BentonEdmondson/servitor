a2bf03f6fc57ad145b64089911861465f4702af4
Benton Edmondson <bentonedmondson@gmail.com>
2023-03-20 22:57:15 -0400

    gemtext: change implementation to using regexps

--- gemtext/gemtext.go
+++ gemtext/gemtext.go
@@ -11,12 +11,6 @@ import (
 	https://gemini.circumlunar.space/docs/specification.html
 */
 
-// TODO: be careful about collapsing whitespace,
-// e.g. block quotes should probably be able to start
-// with tabs
-
-// TODO: change the line parsing to regex-based
-
 func Render(text string, width int) (string, error) {
 	lines := strings.Split(text, "\n")
 	result := ""
@@ -39,47 +33,24 @@ func Render(text string, width int) (string, error) {
 			continue
 		}
 
-		switch {
-		// `^###[ \t]*(.*)$`
-		case strings.HasPrefix(line, "###"):
-			result += style.Header(strings.TrimLeft(line[3:], " \t"), 3) + "\n"
-		// `^##[ \t]*(.*)$`
-		case strings.HasPrefix(line, "##"):
-			result += style.Header(strings.TrimLeft(line[2:], " \t"), 2) + "\n"
-		// `^#[ \t]*(.*)$`
-		case strings.HasPrefix(line, "#"):
-			result += style.Header(strings.TrimLeft(line[1:], " \t"), 1) + "\n"
-		// `^> ?(.*)$`
-		case strings.HasPrefix(line, ">"):
-			/*
-				Don't just TrimLeft all whitespace, because indents should be possible,
-				but at the same time, most people use "> " before their text, so trim
-				a single space if it is present. This is not in the spec but is used
-				in Amfora and presumably others:
-				https://github.com/makeworld-the-better-one/amfora/blob/0b3f874ef19f652fc587ffa80aa6fd08103f892c/renderer/renderer.go#L295
-
-				This could be annoying if someone writes
-					>first line
-					>  second line indented
-				instead of
-					> first line
-					>   second line indented
-			*/
-			result += style.QuoteBlock(strings.TrimPrefix(line[1:], " ")) + "\n"
-		// `^\* (.*)$`
-		case strings.HasPrefix(line, "* "):
-			/*
-				The spec says nothing about optional whitespace, so don't trim at all.
-			*/
-			result += style.Bullet(line[2:]) + "\n"
-		// `^=>[ \t]*(.*?)(?:[ \t]+(.*))?$`
-		case strings.HasPrefix(line, "=>"):
-			rendered, err := renderLink(strings.TrimLeft(line[2:], " \t"))
-			if err != nil {
-				return "", err
+		if match := regexp.MustCompile(`^=>[ \t]*(.*?)(?:[ \t]+(.*))?$`).FindStringSubmatch(line); len(match) == 3 {
+			url := match[1]
+			alt := match[2]
+			if alt == "" {
+				alt = url
 			}
-			result += rendered + "\n"
-		default:
+			result += style.LinkBlock(alt) + "\n"
+		} else if match := regexp.MustCompile(`^#[ \t]+(.*)$`).FindStringSubmatch(line); len(match) == 2 {
+			result += style.Header(match[1], 1) + "\n"
+		} else if match := regexp.MustCompile(`^##[ \t]+(.*)$`).FindStringSubmatch(line); len(match) == 2 {
+			result += style.Header(match[1], 2) + "\n"
+		} else if match := regexp.MustCompile(`^###[ \t]+(.*)$`).FindStringSubmatch(line); len(match) == 2 {
+			result += style.Header(match[1], 3) + "\n"
+		} else if match := regexp.MustCompile(`^\* (.*)$`).FindStringSubmatch(line); len(match) == 2 {
+			result += style.Bullet(match[1]) + "\n"
+		} else if match := regexp.MustCompile(`^> ?(.*)$`).FindStringSubmatch(line); len(match) == 2 {
+			result += style.QuoteBlock(match[1]) + "\n"
+		} else {
 			result += line + "\n"
 		}
 	}
@@ -89,26 +60,5 @@ func Render(text string, width int) (string, error) {
 		result += style.CodeBlock(strings.TrimSuffix(preformattedBuffer, "\n")) + "\n"
 	}
 
-	return strings.TrimSpace(result), nil
-}
-
-func renderLink(text string) (string, error) {
-	/*
-		Regexp to split the line into the url and the optional
-		alt text, while also removing the optional whitespace
-	*/
-	r := regexp.MustCompile(`^(.*?)(?:[ \t]+(.*))?$`)
-	matches := r.FindStringSubmatch(text)
-	url := matches[1]
-	alt := matches[2]
-
-	if alt == "" {
-		alt = url
-	}
-
-	if alt == "" {
-		return text, nil
-	}
-
-	return style.LinkBlock(alt), nil
+	return strings.TrimSuffix(result, "\n"), nil
 }
