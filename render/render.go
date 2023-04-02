0e068261335d7634f6030475b062d7897ddb8be3
Benton Edmondson <bentonedmondson@gmail.com>
2023-04-01 20:41:24 -0400

    render: Render takes pointer to mediaType

--- render/render.go
+++ render/render.go
@@ -8,25 +8,26 @@ import (
 	"mimicry/markdown"
 	"strings"
 	"unicode"
+	"mimicry/jtp"
 )
 
 // TODO: perhaps `dropControlCharacters` should happen to all
 //	`getNatural` strings when they are pulled from the JSON
 // TODO: need to add a width parameter to all of this
-func Render(text string, mediaType string, width int) (string, error) {
+func Render(text string, mediaType *jtp.MediaType, width int) (string, error) {
 	text = strings.Map(dropControlCharacters, text)
 
 	switch {
-	case mediaType == "text/plain": 
+	case mediaType.Full == "text/plain": 
 		return plaintext.Render(text, width)
-	case mediaType == "text/html":
+	case mediaType.Full == "text/html":
 		return hypertext.Render(text, width)
-	case mediaType == "text/gemini":
+	case mediaType.Full == "text/gemini":
 		return gemtext.Render(text, width)
-	case mediaType == "text/markdown":
+	case mediaType.Full == "text/markdown":
 		return markdown.Render(text, width)
 	default:
-		return "", errors.New("Cannot render text of mime type " + mediaType)
+		return "", errors.New("cannot render text of mime type " + mediaType.Full)
 	}
 }
 
