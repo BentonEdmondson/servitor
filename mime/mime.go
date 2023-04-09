1effb1d60a1c32672e3321c7bbedac2dd35b8170
Benton Edmondson <bentonedmondson@gmail.com>
2023-04-08 21:35:40 -0400

    mime: created well-tested mime package for media type handling

---
+++ mime/mime.go
@@ -0,0 +1,55 @@
+package mime
+
+import (
+	"regexp"
+	"errors"
+)
+
+type MediaType struct {
+	Essence string
+	Supertype string
+	Subtype string
+}
+
+/*
+	See: https://httpwg.org/specs/rfc9110.html#media.type
+*/
+var re = regexp.MustCompile(`(?s)^(([!#$%&'*+\-.^_\x60|~a-zA-Z0-9]+)/([!#$%&'*+\-.^_\x60|~a-zA-Z0-9]+)).*$`)
+
+func New() *MediaType {
+	return &MediaType {
+		Essence: "text/html",
+		Supertype: "text",
+		Subtype: "html",
+	}
+}
+
+func Parse(input string) (*MediaType, error) {
+	matches := re.FindStringSubmatch(input)
+
+	if len(matches) != 4 {
+		return nil, errors.New(`"` + input + `" is not a valid media type`)
+	}
+
+	return &MediaType{
+		Essence: matches[1],
+		Supertype: matches[2],
+		Subtype: matches[3],
+	}, nil
+}
+
+func (m *MediaType) Update(input string) error {
+	parsed, err := Parse(input)
+	if err != nil { return err }
+	*m = *parsed
+	return nil
+}
+
+func (m *MediaType) Matches(mediaTypes []string) bool {
+	for _, mediaType := range mediaTypes {
+		if m.Essence == mediaType {
+			return true
+		}
+	}
+	return false
+}
---
+++ mime/mime_test.go
@@ -0,0 +1,71 @@
+package mime
+
+import (
+	"testing"
+)
+
+func TestDefault(t *testing.T) {
+	m := New()
+	if m.Essence != "text/html" {
+		t.Fatalf(`Default media type should be "text/html", not %#v`, m.Essence)
+	}
+	if m.Supertype != "text" {
+		t.Fatalf(`Default media type supertype should be "text", not %#v`, m.Supertype)
+	}
+	if m.Subtype != "html" {
+		t.Fatalf(`Default media type subtype should be "html", not %#v`, m.Subtype)
+	}
+}
+
+func TestFailedParse(t *testing.T) {
+	m, err := Parse("")
+	if err == nil {
+		t.Fatalf("Should fail to parse an empty string, but instead returns: %#v", m)
+	}
+
+	m, err = Parse("application")
+	if err == nil {
+		t.Fatalf("Should fail to parse invalid media type, but instead returns: %#v", m)
+	}
+}
+
+func TestSuccessfulUpdate(t *testing.T) {
+	m := New()
+	err := m.Update("application/json ; charset=utf-8")
+	if err != nil {
+		t.Fatalf("Update should have succeeded but returned error: %#v", err)
+	}
+	if m.Essence != "application/json" {
+		t.Fatalf(`New media type should be "application/json", not %#v`, m.Essence)
+	}
+	if m.Supertype != "application" {
+		t.Fatalf(`New media type supertype should be "application", not %#v`, m.Supertype)
+	}
+	if m.Subtype != "json" {
+		t.Fatalf(`New media type subtype should be "json", not %#v`, m.Subtype)
+	}
+}
+
+func TestFailedUpdate(t *testing.T) {
+	m := New()
+	err := m.Update("no slash")
+	if err == nil {
+		t.Fatalf(`Expected "no slash" to result in an Update error, but it resulted in: %#v`, m)
+	}
+}
+
+func TestMatchesSuccess(t *testing.T) {
+	m := New()
+	matches := m.Matches([]string{"application/json", "text/html"})
+	if !matches {
+		t.Fatalf(`Expected media type to match text/html but it did not: %#v`, m)
+	}
+}
+
+func TestMatchesFailure(t *testing.T) {
+	m := New()
+	matches := m.Matches([]string{"application/json"})
+	if matches {
+		t.Fatalf(`Expected media type to not match application/json: %#v`, m)
+	}
+}
